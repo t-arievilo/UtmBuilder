aca1efd654a91aee000e0509e9591115b42f3ec7
Thiago Oliveira <thiago.math@hotmail.com>
2026-02-27 12:39:49 -0300

    refatora a classe InvalidUrlException

--- UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
+++ UtmBuilder.Core/ValueObjects/Exceptions/InvalidUrlException.cs
@@ -1,9 +1,27 @@
-﻿namespace UtmBuilder.Core.ValueObjects;
+﻿using System.Text.RegularExpressions;
 
-public class InvalidUrlException : Exception
+namespace UtmBuilder.Core.ValueObjects;
+
+public partial class InvalidUrlException : Exception
 {
-    public InvalidUrlException(string message = "Invalid url") : base(message)
+    private const string DefaultErrorMessage = "Invalid url";
+
+    public InvalidUrlException(string message = DefaultErrorMessage) : base(message)
     {
         
     }
+
+    public static void ThrowIfInvalid(string address, string message = DefaultErrorMessage)
+    {
+        if(string.IsNullOrEmpty(address))
+            throw new InvalidUrlException(message);
+        
+        if(!UrlRegex().IsMatch(address))
+                    throw new InvalidUrlException();
+    }
+
+    [GeneratedRegex(
+        @"^(http|https):(\/\/www\.|\/\/|/\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$|(http|https):(\/\/localhost:\d*|\/\/127\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\/.*)?$")]
+
+    private static partial Regex UrlRegex();
 }
