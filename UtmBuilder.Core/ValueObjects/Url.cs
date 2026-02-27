93f0a2bf3972a2859fd8d484fae5b9bd692fb545
Thiago Oliveira <thiago.math@hotmail.com>
2026-02-27 12:11:16 -0300

    adiciona Regex para validar Url

--- UtmBuilder.Core/ValueObjects/Url.cs
+++ UtmBuilder.Core/ValueObjects/Url.cs
@@ -1,10 +1,16 @@
-﻿namespace UtmBuilder.Core.ValueObjects;
+﻿using System.Text.RegularExpressions;
+
+namespace UtmBuilder.Core.ValueObjects;
 
 public class Url : ValueObjects
 {
+    private const string UrlRegexPattern =
+        @"^(http|https):(\/\/www\.|\/\/|/\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$|(http|https):(\/\/localhost:\d*|\/\/127\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\/.*)?$";
     public Url(string address)
     {
         Address = address;
+        if(Regex.IsMatch(Address, UrlRegexPattern))
+            throw new Exception("Teste");
     }
     
     
