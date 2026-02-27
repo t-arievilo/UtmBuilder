ff9505696e56f7fab9431e9efef8681377eb11ad
Thiago Oliveira <thiago.math@hotmail.com>
2026-02-27 17:55:46 -0300

    adiciona implicit operations e cria methodo para concatenar url

--- UtmBuilder.Core/Extensions/ListExtensions.cs
+++ UtmBuilder.Core/Extensions/ListExtensions.cs
@@ -1,9 +1,9 @@
 ﻿namespace UtmBuilder.Core.Extensions;
-
 public static class ListExtensions
 {
-    public static void AddIfNotNull(this List<string> list)
+    public static void AddIfNotNull(this List<string> list, string key, string? value)
     {
-        
+        if (!string.IsNullOrWhiteSpace(value))
+            list.Add($"{key}={value}");
     }
 }
