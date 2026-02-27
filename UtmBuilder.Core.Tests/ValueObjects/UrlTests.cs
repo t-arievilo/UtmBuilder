e31b12af543c65da5ee0005c4ece0b2362925d6d
Thiago Oliveira <thiago.math@hotmail.com>
2026-02-27 17:56:16 -0300

    cria e executa os testes unitarios para Url

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,20 @@
+﻿using UtmBuilder.Core.ValueObjects;
+
+namespace UtmBuilder.Core.Tests.ValueObjects;
+
+[TestClass]
+public class UrlTests
+{
+    [TestMethod]
+    [ExpectedException(typeof(InvalidUrlException))]
+    public void Dado_uma_url_invalida_deve_retornar_uma_excecao()
+    {
+        new Url("banana");
+    }
+    [TestMethod]
+    public void Dado_uma_url_valida_nao_deve_retornar_uma_excecao()
+    {
+        new Url("https://google.com");
+        Assert.IsTrue(true);
+    }
+}
